33ac1fa25ff43a67ac32822ae7147c410c58d118
Hy-Vong <hyvong.georges@gmail.com>
2019-03-07 13:32:57 -0500

    Maquette Liste + script js general
    Ajout de la maquette pour la page Liste.
    Ajout d'un fichier de script footer.js permettant de gérer les liens sociaux sur toutes les pages du site.

---
+++ www/footer.js
@@ -0,0 +1,31 @@
+function remplacerLienParImage()
+{
+	var lienFacebook = new Image();
+	var lienTwitter = new Image();
+	var lienInstagram = new Image();
+
+	lienFacebook.src = './decoration/facebook.png';
+	lienTwitter.src = './decoration/twitter.jpg';
+	lienInstagram.src = './decoration/instagram.png';
+
+	lienFacebook.onclick = function()
+	{
+   		window.location.href = 'https://www.facebook.com/';
+	};
+	lienTwitter.onclick = function()
+	{
+	 	window.location.href = 'https://www.twitter.com/';
+	};
+	lienInstagram.onclick = function()
+	{
+ 		window.location.href = 'https://www.instagram.com/';
+	};
+
+	lienFacebook.style.size = '45px';
+	lienTwitter.style.size = '45px';
+	lienInstagram.style.size = '45px';
+
+	document.getElementById("lien-facebook").appendChild(lienFacebook);
+	document.getElementById("lien-twitter").appendChild(lienTwitter);
+	document.getElementById("lien-instagram").appendChild(lienInstagram);
+}
